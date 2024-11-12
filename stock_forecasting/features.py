16a16cfa064471dda60e889b46ae0c4343b3aa5e
PsuperX <ja435364@gmail.com>
2024-11-12 15:32:18 +0000

    wavelet + testing pipeline

--- stock_forecasting/features.py
+++ stock_forecasting/features.py
@@ -1,28 +1,47 @@
 from pathlib import Path
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
 
 import typer
 from loguru import logger
 from tqdm import tqdm
 
 from .config import PROCESSED_DATA_DIR
+from .utils import wavelet_transform, waveletSmooth
 
 app = typer.Typer()
 
+class WaveletTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, level=1, wavelet='db1'):
+        self.level = level
+        self.wavelet = wavelet
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
+        self.prev_x = None
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        if self.prev_x is None:
+            X_transformed = np.copy(X)
+            for i in range(X.shape[1]):
+                X_transformed[:, i] = waveletSmooth(X[:, i], level=self.level, wavelet=self.wavelet)[-len(X):]
+
+            self.prev_x = X_transformed
+            return X_transformed
+        
+        else:
+            # for the validation we have to do the transform using training data + the current and past validation data
+            # i.e. we CAN'T USE all the validation data because we would then look into the future 
+            temp = np.copy(self.prev_x)
+            X_test_WT = np.copy(X)
+            for j in range(X.shape[0]):
+                #first concatenate train with the latest validation sample
+                temp = np.append(temp, np.expand_dims(X[j,:], axis=0), axis=0)
+                for i in range(X.shape[1]):
+                    X_test_WT[j,i] = waveletSmooth(temp[:,i], level=1)[-1]
+
+            return X_test_WT
 
 
 if __name__ == "__main__":
--- stock_forecasting/modeling/train.py
+++ stock_forecasting/modeling/train.py
@@ -1,12 +1,54 @@
 from pathlib import Path
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_regression
+from sklearn.model_selection import BaseCrossValidator
+import numpy as np
+
 
 import typer
 from loguru import logger
 from tqdm import tqdm
 
 from stock_forecasting.config import MODELS_DIR, PROCESSED_DATA_DIR
+from stock_forecasting.features import WaveletTransformer
 
 app = typer.Typer()
+
+
+class CustomTimeSeriesSplit(BaseCrossValidator):
+    def __init__(self, n_splits=5, train_size=None):
+        self.n_splits = n_splits
+        self.train_size = train_size
+    
+    def split(self, X, y=None, groups=None):
+        # Check that the input data is at least of the required size
+        n_samples = len(X)
+        
+        if self.train_size is None:
+            self.train_size = n_samples // (self.n_splits + 1)
+        
+        # Ensure we have enough samples to perform the split
+        if n_samples <= self.train_size * self.n_splits:
+            raise ValueError("The number of samples is too small for the number of splits.")
+        
+        # Generate the splits
+        for i in range(self.n_splits):
+            train_end = self.train_size * (i + 1)  # The end of the training set
+            test_start = train_end  # The start of the test set
+            
+            if test_start + self.train_size > n_samples:
+                test_start = n_samples - self.train_size
+
+            train_indices = np.arange(train_end)
+            test_indices = np.arange(test_start, min(test_start + self.train_size, n_samples))
+            
+            yield train_indices, test_indices
+
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self.n_splits
+
+
 
 
 @app.command()
@@ -17,13 +59,18 @@ def main(
     model_path: Path = MODELS_DIR / "model.pkl",
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+    X, y = make_regression(n_samples=100, n_features=100, n_targets=1)
+
+    pipeline = Pipeline([
+        ("preprocessing", StandardScaler()),
+        ("wavelet", WaveletTransformer())
+        #("classifier", )
+    ])
+
+    pipeline.fit(X[:-10], y[:-10])
+
+
+
 
 
 if __name__ == "__main__":
